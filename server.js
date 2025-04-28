869b0b86e5874e151e9e31f365b9a1ad8e6e84ff
salvestacia <darrensiwibu@gmail.com>
2025-04-28 15:28:52 +0700

    new serer.js

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const port = 3000;
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello, World, Salve, Stacia, Christy, Dionisius, Alexander, and Darren Christian!');
+  res.end('Hello World, Andi, Salve, Stacia, Christy, Dionisius, Alexander, and Darren Christian!');
 });
 
 server.listen(port, hostname, () => {

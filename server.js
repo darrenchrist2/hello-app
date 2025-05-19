878947a7b9e1d19490c756c832cc6a77b920d758
salvestacia <darrensiwibu@gmail.com>
2025-05-19 14:23:48 +0700

    new response message

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const port = 3000;
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello World, then Hello Sky, then Hello People!');
+  res.end('Hello World, then Hello Sky, then Hello People,then Hello Everyone !');
 });
 
 server.listen(port, hostname, () => {
